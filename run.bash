82d19fb6ba72a18c42ceafc9df8810be3469a1fb
Trenton Laughlin <157517803+Tdlaughl@users.noreply.github.com>
2024-02-27 12:10:49 -0600

    Update run.bash

--- run.bash
+++ run.bash
@@ -3,3 +3,5 @@ sudo apt-get install libsfml-dev
 sudo apt install nlohmann-json3-dev
 sudo apt install g++
 sudo apt-get install libcurl4-openssl-dev
+sudo apt install ttf-mscorefonts-installer
+sudo fc-cache -f
