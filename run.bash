c70b6142710f993ad1ae6498a26d4fe6be3b2547
Trenton Laughlin <157517803+Tdlaughl@users.noreply.github.com>
2024-03-13 20:41:38 -0500

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,8 +1,26 @@
 #!/bin/bash
+
+# Update package lists
 sudo apt update
-sudo apt-get install libsfml-dev
-sudo apt install nlohmann-json3-dev
-sudo apt install g++
-sudo apt-get install libcurl4-openssl-dev
-sudo apt install ttf-mscorefonts-installer
+sleep 5
+
+# Install SFML
+sudo apt-get install -y libsfml-dev
+sleep 5
+
+# Install nlohmann-json library
+sudo apt install -y nlohmann-json3-dev
+sleep 5
+
+# Install g++
+sudo apt install -y g++
+sleep 5
+
+# Install libcurl
+sudo apt-get install -y libcurl4-openssl-dev
+sleep 5
+
+# Install Microsoft TrueType core fonts
+sudo apt install -y ttf-mscorefonts-installer
 sudo fc-cache -f
+sleep 5
