5a7a64fe3bec9d028b7efb8e28ae5886559d4b2d
Trenton Laughlin <157517803+Tdlaughl@users.noreply.github.com>
2024-03-14 10:21:47 -0500

    Update and rename run.bash to run.sh

--- run.sh
+++ run.sh
@@ -2,25 +2,19 @@
 
 # Update package lists
 sudo apt update
-sleep 5
 
 # Install SFML
 sudo apt-get install -y libsfml-dev
-sleep 5
 
 # Install nlohmann-json library
 sudo apt install -y nlohmann-json3-dev
-sleep 5
 
-# Install g++
+# Install g++ (if not installed)
 sudo apt install -y g++
-sleep 5
 
-# Install libcurl
+# Install libcurl (if not installed)
 sudo apt-get install -y libcurl4-openssl-dev
-sleep 5
 
-# Install Microsoft TrueType core fonts
+# Install Microsoft TrueType core fonts (if not installed)
 sudo apt install -y ttf-mscorefonts-installer
 sudo fc-cache -f
-sleep 5
