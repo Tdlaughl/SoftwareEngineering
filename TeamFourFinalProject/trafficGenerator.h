a9ed9ae9fe74982173823e274933119509626f80
Trenton Laughlin <157517803+Tdlaughl@users.noreply.github.com>
2024-04-21 16:10:14 -0500

    Add files via upload

---
+++ TeamFourFinalProject/trafficGenerator.h
@@ -0,0 +1,10 @@
+#ifndef TRAFFIC_GENERATOR_H
+#define TRAFFIC_GENERATOR_H
+
+// Include necessary headers
+#include <string>
+
+// Declare the function
+int generateTraffic();
+
+#endif // TRAFFIC_GENERATOR_H
